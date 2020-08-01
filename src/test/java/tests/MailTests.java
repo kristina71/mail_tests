27054f1df1c3ef2d81c6156c542090571977b155
Кристина <kristina@MacBook-Pro-Kristina.local>
2020-08-01 22:29:22 +0300

    Add socket tests

--- src/test/java/tests/MailTests.java
+++ src/test/java/tests/MailTests.java
@@ -5,19 +5,21 @@ import elements.LoginWidget;
 import elements.ProfileWidget;
 import helpers.LoginHelper;
 import helpers.MailHelper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selenide.*;
-import static utils.RandomUtils.getRandomMessage;
-import static utils.RandomUtils.getRandomString;
-
 import pages.LoginPage;
 import pages.MailPage;
+import utils.EmailUtils;
+
+import static com.codeborne.selenide.Selenide.open;
+import static utils.RandomUtils.getRandomMessage;
+import static utils.RandomUtils.getRandomString;
 
 
 public class MailTests extends TestBase{
 
     @Test
+    @DisplayName("Login and check userName")
     public void UIAuthMailTest(){
         final String url = "https://account.mail.ru/";
         open(url);
@@ -33,6 +35,7 @@ public class MailTests extends TestBase{
     }
 
     @Test
+    @DisplayName("Send message")
     public void UIMailSendMessageTest() {
         final String url = "https://account.mail.ru/";
         open(url);
@@ -56,6 +59,7 @@ public class MailTests extends TestBase{
     }
 
     @Test
+    @DisplayName("Send message and check letter")
     public void UIMailSendMessageAndCheckLetterTest() {
         final String url = "https://account.mail.ru/";
         open(url);
@@ -94,5 +98,58 @@ public class MailTests extends TestBase{
         profileWidget2.checkName(userName2);
 
         mailPage.getMessageWidget().findMessageBySubject(subject);
+    }
+
+    //Не рабоает =(
+    @Test
+    @DisplayName("Отправляем (UI) и получаем почту (API)")
+    void sendUIReceiveApiMailTest() {
+        final String url = "https://account.mail.ru/";
+        open(url);
+        LoginWidget loginWidget = new LoginPage().getLoginWidget();
+
+        final String userName1 = "kristina71-00";
+        final String userPassword1 = "obYRtaMPp34*";
+
+        final String userName2 = "kristina71-11";
+        final String userPassword2 = "obYRtaMPp34*";
+
+        LoginHelper.signIn(userName1,userPassword1,loginWidget);
+
+        ProfileWidget profileWidget= new MailPage().getProfileWidget();
+        profileWidget.checkName(userName1);
+
+        ComposePopupWidget composePopupWidget = new MailPage().getComposeWidget().clickComposeBtn();
+
+        final String sendTo = String.format("%s@mail.ru",userName2);
+        final String subject = getRandomString(10);
+        final String message = getRandomMessage(10, 20);
+
+        MailHelper.sendMail(sendTo,subject,message,composePopupWidget);
+
+        final String successText = "Письмо отправлено";
+        composePopupWidget.checkSuccessMessage(successText);
+
+        //Не рабоает =(
+        new EmailUtils().verifyMailHasMessage(userName2+"@mail.ru", userPassword2, subject);
+    }
+
+
+    //Не рабоает =(
+    @Test
+    @DisplayName("Отправляем (API) и получаем почту (API)")
+    void sendApiAndReceiveApiMailTest() {
+        String randomMessage = getRandomMessage(30, 60);
+
+        final String userName1 = "kristina71-00";
+        final String userPassword1 = "obYRtaMPp34*";
+
+        final String userName2 = "kristina71-11";
+        final String userPassword2 = "obYRtaMPp34*";
+
+        new EmailUtils().sendMail(userName1+"@mail.ru", userPassword1, userName2+"@mail.ru", "", randomMessage);
+
+        new EmailUtils().verifyMailHasMessage(userName2+"@mail.ru", userPassword2, randomMessage);
+
     }
 }
